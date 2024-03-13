78971d33b6f5c1b53b09565203e2acaec128a3a7
TheOnlyJason <141594054+TheOnlyJason@users.noreply.github.com>
2024-03-12 20:10:01 -0700

    Initial Commit

---
+++ homework7pic10B/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  homework7pic10B
+//
+//  Created by Jason Dai on 3/12/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
